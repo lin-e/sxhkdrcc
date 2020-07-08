e20ea3ce6823d811ba9648a1d6a9323c89ac3b2f
me@eugenel.in <me@eugenel.in>
2020-07-08 02:56:13 +0100

    Implement selectors and basic placeholders, along with main program parsing

--- src/main/scala/sxhkdrcc/Compiler.scala
+++ src/main/scala/sxhkdrcc/Compiler.scala
@@ -13,11 +13,39 @@ object Main {
   def main(args: Array[String]): Unit = {
     val ident = token(some(alphaNum).map(_.mkString))
 
+    val substitute = "#" *> ident <#> Substitute
+
+    val nothing = "_" #> Nothing
+
+    lazy val selectTrig: Parsley[Trigger] = ("{" *> sepBy1(nothing <|> trigger, ",") <* "}") <#> TSelect
+    lazy val selectComm: Parsley[Command] = ("{" *> sepBy1(command, ",") <* "}") <#> CSelect
+
+    lazy val trigger = substitute <|> selectTrig <|> ("gtrig" #> Trig)
+    lazy val command = substitute <|> selectComm <|> ("gcomm" #> Comm)
 
     val declare = lift2(Declare.apply, "let" *> ident, "=" *> ident)
+    val binding = lift2(Binding.apply, sepBy1(trigger, "+"), ":" *>  many(command))
+
+    val prog = sepEndBy1(declare <|> binding, some(endOfLine)) <* many(whitespace) <* notFollowedBy(anyChar)
+
+    val src =
+      """let primary = alt
+        |let secondary = super
+        |let arg1 = q
+        |let arg2 = r
+        |#primary + #secondary + {_, gtrig}:     gcomm {#arg1, #arg2}
+        |#primary + gtrig:                       gcomm #arg1 #arg2
+      """.stripMargin
+
+    runParser(prog, src) match {
+      case Success(x) => print(x)
+      case Failure(msg) => print(msg)
+    }
   }
 
   implicit def charToken(c: Char): Parsley[Char] = token(char(c))
-  implicit def stringToken(s: String): Parsley[String] = token(string(s) <* notFollowedBy(char('_') <|> alphaNum))
-  def token[A](t: => Parsley[A]): Parsley[A] = attempt(t) <* many(whitespace)
+  implicit def stringToken(s: String): Parsley[String] = token(string(s))
+  def token[A](t: => Parsley[A]): Parsley[A] = attempt(t) <* many(
+    char(' ') <|> char('\t') <|> char('\f') <|> char('\u000b')
+  )
 }
--- src/main/scala/sxhkdrcc/Nodes.scala
+++ src/main/scala/sxhkdrcc/Nodes.scala
@@ -6,7 +6,23 @@ object Nodes {
 
   case class Declare(identifier: String, value: String) extends Statement
 
-  case class Binding(trigger: Trigger, body: String) extends Statement
+  case class Binding(trigger: List[Trigger], body: List[Command]) extends Statement
 
   sealed trait Trigger
+
+  case object Trig extends Trigger
+
+  case object Nothing extends Trigger
+
+  case class TSelect(options: List[Trigger]) extends Trigger
+
+  sealed trait Command
+
+  case object Comm extends Command
+
+  case class Literal(value: String) extends Command
+
+  case class CSelect(options: List[Command]) extends Command
+
+  case class Substitute(identifier: String) extends Trigger with Command
 }
